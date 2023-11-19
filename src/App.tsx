7908cddf6195c57f1e13b9873fc12f262e8bc4c4
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-11-19 17:36:18 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <Container sx={{ bgcolor: '#FFEEFC', height: '100vh' }}>
+      <Header />
+    </Container>
+  );
+}
+
+export default App;
